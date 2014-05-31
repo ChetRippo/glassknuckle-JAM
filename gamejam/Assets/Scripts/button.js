b0fd6aa94bae77e893713c0ccf0546e7ac5170a9
vibrating-orb <kfleisc@gmail.com>
2014-05-31 14:33:21 -0400

    map

---
+++ gamejam/Assets/Scripts/button.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+	var btnTexture : Texture;
+	function OnGUI() {
+		if (!btnTexture) {
+			Debug.LogError("texture needed");
+			return;
+		}
+		GUI.backgroundColor = new Color(0,0,0,0);
+		if (GUI.Button(Rect(10,10,500,500),btnTexture))
+			Debug.Log("Clicked the button with an image");
+			Application.LoadLevel ("date");
+		}
+
+function Start () {
+
+}
+
+function Update () {
+
+}
